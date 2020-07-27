44d05bcd835d1f3940e49ec097316d6dba49a1e6
abun880007 <abun880007@msn.com>
2020-07-27 21:40:21 +0100

    update for 420rom

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ To get started with building GSIs
 
 First, open a new Terminal window, which defaults to your home directory.  Clone the modified treble_experimentations repo there:
 
-    git clone https://github.com/420rom/treble_experimentations
+    git clone ssh://git@github.com/420rom/treble_experimentations
 
 Create a new working directory for your 420rom build and navigate to it:
 
@@ -13,12 +13,12 @@ Create a new working directory for your 420rom build and navigate to it:
 
 Initialize your 420rom workspace:
 
-    repo init -u https://github.com/420rom/android.git -b 420rom-10
+    repo init -u ssh://git@github.com/420rom/android.git -b 420rom-10
 
 Clone the modified treble patches and this repo:
 
-    git clone https://github.com/420rom/treble_patches -b 420rom-10
-    git clone https://github.com/420rom/treble_build_los -b 420rom-10
+    git clone ssh://git@github.com/420rom/treble_patches -b 420rom-10
+    git clone ssh://git@github.com/420rom/treble_build_420rom -b 420rom-10
 
 Finally, start the build script:
 
--- buildbot_treble.sh
+++ buildbot_treble.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+echo "                                                   "
+echo "      420rom Pixel Edition Rom v5.0 Builder        "
+echo "                                                   "
+echo "       Perfect base for the 420rom module          "
+echo "                                                   "
+echo "            By ExocetDJ & Abun880007               "
+echo "                                                   "
+echo "         Get the Download links from our           "
+echo "       Telegram Group t.me/Home_of_420roms         "
 echo ""
 echo "420rom Treble Buildbot"
 echo "ATTENTION: this script syncs repo on each run"
@@ -24,13 +33,13 @@ source build/envsetup.sh &> /dev/null
 echo ""
 
 echo "Reverting LOS FOD implementation"
-cd frameworks/base
+cd ~/build/420rom/frameworks/base
 git am $BL/patches/0001-Squashed-revert-of-LOS-FOD-implementation.patch
 cd ../..
-cd frameworks/native
+cd ~/build/420rom/frameworks/native
 git am $BL/patches/0001-Revert-surfaceflinger-Add-support-for-extension-lib.patch
 cd ../..
-cd vendor/420rom
+cd ~/build/420rom/vendor/420rom
 git revert 612c5a846ea5aed339fe1275c119ee111faae78c --no-edit # soong: Add flag for fod extension
 cd ../..
 echo ""
@@ -41,53 +50,53 @@ cd device/phh/treble
 git clean -fdx
 bash generate.sh 420rom
 cd ../../..
-bash ~/treble_experimentations/apply-patches.sh treble_patches
+bash ~/build/treble_experimentations/apply-patches.sh treble_patches
 echo ""
 
 echo "Applying universal patches"
-cd frameworks/base
+cd ~/build/420rom/frameworks/base
 git am $BL/patches/0001-UI-Revive-navbar-layout-tuning-via-sysui_nav_bar-tun.patch
 git am $BL/patches/0001-Disable-vendor-mismatch-warning.patch
 git am $BL/patches/0001-core-Add-support-for-MicroG.patch
 cd ../..
-cd lineage-sdk
+cd ~/build/420rom/lineage-sdk
 git am $BL/patches/0001-sdk-Invert-per-app-stretch-to-fullscreen.patch
 cd ..
-cd packages/apps/LineageParts
+cd ~/build/420rom/packages/apps/LineageParts
 git am $BL/patches/0001-LineageParts-Invert-per-app-stretch-to-fullscreen.patch
 cd ../../..
-cd vendor/420rom
-git am $BL/patches/0001-vendor_lineage-Log-privapp-permissions-whitelist-vio.patch
+cd ~/build/420rom/vendor/420rom
+git am $BL/patches/0001-vendor_420rom-Log-privapp-permissions-whitelist-vio.patch
 cd ../..
 echo ""
 
 echo "Applying GSI-specific patches"
-cd build/make
+cd ~/build/420rom/build/make
 git am $BL/patches/0001-build-Don-t-handle-apns-conf.patch
 cd ../..
-cd device/phh/treble
+cd ~/build/420rom/device/phh/treble
 git revert 82b15278bad816632dcaeaed623b569978e9840d --no-edit # Update lineage.mk for LineageOS 16.0
 git am $BL/patches/0001-Remove-fsck-SELinux-labels.patch
 git am $BL/patches/0001-treble-Add-overlay-lineage.patch
 git am $BL/patches/0001-treble-Don-t-specify-config_wallpaperCropperPackage.patch
 git am $BL/patches/0001-TEMP-treble-Fix-init.treble-environ.rc-hardcode-for-.patch
 cd ../../..
-cd external/tinycompress
+cd ~/build/420rom/external/tinycompress
 git revert 82c8fbf6d3fb0a017026b675adf2cee3f994e08a --no-edit # tinycompress: Use generated kernel headers
 cd ../..
-cd frameworks/native
+cd ~/build/420rom/frameworks/native
 git revert 581c22f979af05e48ad4843cdfa9605186d286da --no-edit # Add suspend_resume trace events to the atrace 'freq' category.
 cd ../..
-cd hardware/420rom/interfaces
+cd ~/build/420rom/hardware/420rom/interfaces
 git am $BL/patches/0001-cryptfshw-Remove-dependency-on-generated-kernel-head.patch
 cd ../../..
-cd system/hardware/interfaces
+cd ~/build/420rom/system/hardware/interfaces
 git revert 5c145c49cc83bfe37c740bcfd3f82715ee051122 --no-edit # system_suspend: start early
 cd ../../..
-cd system/sepolicy
+cd ~/build/420rom/system/sepolicy
 git revert d12551bf1a6e8a9ece6bbb98344a27bde7f9b3e1 --no-edit # sepolicy: Relabel wifi. properties as wifi_prop
 cd ../..
-cd vendor/420rom
+cd ~/build/420rom/vendor/420rom
 git am $BL/patches/0001-build_soong-Disable-generated_kernel_headers.patch
 cd ../..
 echo ""
@@ -97,7 +106,7 @@ sleep 5
 echo ""
 
 export WITHOUT_CHECK_API=true
-export WITH_SU=true
+export WITH_SU=false
 mkdir -p ~/build-output/
 
 buildVariant() {
@@ -108,18 +117,11 @@ buildVariant() {
 	mv $OUT/system.img ~/build-output/420rom-10-$BUILD_DATE-OFFICIAL-${1}.img
 }
 
-buildVariant treble_arm_avN
-buildVariant treble_arm_bvN
-buildVariant treble_a64_avN
-buildVariant treble_a64_bvN
-buildVariant treble_arm64_avN
 buildVariant treble_arm64_agN
-buildVariant treble_arm64_bvN
-buildVariant treble_arm64_bgN
 ls ~/build-output | grep '420rom'
 
+echo "       420rom Pixel Edition Rom v5.0 Build Completed         "
 END=`date +%s`
 ELAPSEDM=$(($(($END-$START))/60))
 ELAPSEDS=$(($(($END-$START))-$ELAPSEDM*60))
 echo "Buildbot completed in $ELAPSEDM minutes and $ELAPSEDS seconds"
-echo ""
