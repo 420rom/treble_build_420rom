cdd60092cd6ecf2509ab3e6459d7efeff334e90d
abun880007 <abun880007@msn.com>
2020-10-16 20:08:33 +0100

    move to bgN

--- buildbot_treble.sh
+++ buildbot_treble.sh
@@ -115,16 +115,9 @@ buildVariant() {
 	make installclean
 	make -j$(nproc --all) systemimage
 	make vndk-test-sepolicy
-	mv $OUT/system.img ~/build-output/420rom-10-$BUILD_DATE-OFFICIAL-${1}.img
+	mv $OUT/system.img ~/build-output/420rom-11-$BUILD_DATE-OFFICIAL-${1}.img
 }
 
-buildVariant treble_arm_avS
-buildVariant treble_arm_bvS
-buildVariant treble_a64_avS
-buildVariant treble_a64_bvS
-buildVariant treble_arm64_avS
-buildVariant treble_arm64_bvS
-buildVariant treble_arm64_agN
 buildVariant treble_arm64_bgN
 ls ~/build-output | grep '420rom'
 
